40949f9d76151e5e5a6e505237569cd61822e3b3
khanifnaufal <mkhanif86@gmail.com>
2026-03-25 12:27:02 +0700

    implement navigation

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import React from 'react'
+import Image from 'next/image';
+import  Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+
+
+const Header = () => {
+  const pathname = usePathname();
+  return (
+    <header>
+    <div className="main-container inner">
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="CuanWatch Logo"
+          width={200}
+          height={40}
+        />
+      </Link>
+      <nav>
+        <Link rel="stylesheet" href="/" className={cn('nav-link',
+          {'is-active' : pathname == '/',
+            'is-home' : true
+          } )}
+          > Home </Link>
+        <p>Search Modal</p>
+        <Link href="/coins" className={cn('nav-link',
+          {'is-active' : pathname == '/coins',
+            'is-home' : false
+          } )}> All Coins </Link>
+      </nav>
+    </div>
+    </header>
+
+  )
+}
+
+export default Header
